af3212667b9769e9ce1cfab3c659b567b657095c
ArwaDagher <arwa.daghir@gmail.com>
2016-04-08 13:20:57 +0200

    Course4Project1Updated

--- plot3.R
+++ plot3.R
@@ -9,12 +9,13 @@ table <- table[(table$Date == "2007-02-01") | (table$Date == "2007-02-02"),]
 
 table$DateTime <- as.POSIXct(paste(table$Date, table$Time))
 
+png("plot3.png", width=480, height=480)
+
 plot(table$Sub_metering_1~table$DateTime, type="l", col="black", xlab="", ylab = "Energy Sub metering")
 lines(table$DateTime, table$Sub_metering_2, col="red")
 lines(table$DateTime, table$Sub_metering_3, col="blue")
 
 legend("topright",  col=c("black", "red", "blue"),
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, cex = 0.7)
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1)
 
-dev.copy(png, file="plot3.png", width=480, height=480)
 dev.off()
--- plot4.R
+++ plot4.R
@@ -5,9 +5,9 @@ table$Date <- as.Date(table$Date, format = "%d/%m/%Y")
 
 table <- table[(table$Date == "2007-02-01") | (table$Date == "2007-02-02"),]
 
-
-
 table$DateTime <- as.POSIXct(paste(table$Date, table$Time))
+
+png("plot4.png", width=480, height=480)
 par(mfrow=c(2,2), mar=c(2,4,2,1))
 plot(table$Global_active_power~table$DateTime, type="l", ylab = "Global Active Power", xlab="")
 plot(table$Voltage~table$DateTime, type="l", ylab = "Voltage", xlab="datetime")
@@ -16,11 +16,8 @@ lines(table$DateTime, table$Sub_metering_2, col="red")
 lines(table$DateTime, table$Sub_metering_3, col="blue")
 
 legend("topright",  col=c("black", "red", "blue"),
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, cex = 0.7)})
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1)})
 
 plot(table$Global_reactive_power~table$DateTime, type="l", ylab = "global_reactive_power", xlab="datetime")
 
-
-
-dev.copy(png, file="plot4.png", width=480, height=480)
 dev.off()
